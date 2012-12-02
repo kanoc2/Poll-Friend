4650f06bccba9261a98892a04d528a8f5c34afef
Brandon Lewis <lewisb3@CSMac9.local>
2012-12-02 16:15:58 -0500

    user.java file for user accounts

---
+++ user.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+public class user	{
+	String name;
+	String password;
+	Hashtable taken;
+	BufferedReader in;
+	BufferedWriter out;
+	
+	//This is the default for a new user
+	//It asks for a username and password
+	public user()	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter new Username");
+		name = kb.next();
+		password = kb.next();
+		taken = new Hashtable();
+	}
+	
+	//This is when a user tries to login with a username
+	//The program loads the polls taken from the text file
+	public user(String n)	throws java.io.IOException	{
+		name = n;
+		taken = new Hashtable();
+		try	{
+		in = new BufferedReader(new FileReader(name + ".txt"));
+		while (in.ready())	{
+			taken.put(in.readLine(), 1);
+		}
+		} catch (FileNotFoundException ex)	{
+			System.err.println(ex);
+		}
+	}
+	
+	//Unfinished
+	public boolean validate(String pw)	{
+		return true;
+	}
+	
+	//checks to see if a user has been polled from a certain poll
+	//For a given string, see if it is in a hashtable and return that. 
+	public boolean hasPolled(String name)	{
+		return taken.containsKey(name);
+	}
+	
+	//The user has just taken a new poll, and the hashtable needs to be updated
+	public void tookPoll(String st)	{
+		taken.put(st, 1);
+	}
+	
+	//When a user logs out, their file needs to be updated.
+	public void store()	throws java.io.IOException {
+		Set temp = taken.keySet();
+		Iterator tmp = temp.iterator();
+		out = new BufferedWriter(new FileWriter(new File(name + ".txt"), true));;
+		String fullstring = "";
+		while (tmp.hasNext())	{
+			fullstring.concat(tmp.next().toString());
+			fullstring.concat("/n");
+		}
+		
+		out.write(fullstring, 0, fullstring.length());
+	}
+	
+	
+
+}
